8a8107624e9a42b76ddaf8676d0b6dfdc3dd0c79
smythlamar <caltiesmith41@gmail.com>
2023-07-19 05:48:53 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,14 @@
 
 void print_alphabet_x10(void)
 {
-        char num, letter;
-
-        for (num = 0; num <= 9; num++)
-        {
-                for (letter = 'a'; letter <= 'z'; letter++)
-                {
-                        _putchar(letter);
-                }
-                _putchar('\n');
-        }
+	char num, letter;
+	
+	for (num = 0; num <= 9; num++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
 }
