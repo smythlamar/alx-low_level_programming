7b13dcf8bc7cdaa789ea115bec50ccf8f8150355
smythlamar <caltiesmith41@gmail.com>
2023-07-22 15:14:38 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_number - func checks for a digit (0 through)
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for(c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
