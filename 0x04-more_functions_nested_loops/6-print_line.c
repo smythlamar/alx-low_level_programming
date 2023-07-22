6c7ab42cc33e88e532fc72d4df7c39568db1c2d4
smythlamar <caltiesmith41@gmail.com>
2023-07-22 16:53:02 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *print_line - func draws a straight line on the terminal
- *@n: is the number of times _ should be printed
+ *@x: is the number of times _ should be printed
  */
 void print_line(int x)
 {
