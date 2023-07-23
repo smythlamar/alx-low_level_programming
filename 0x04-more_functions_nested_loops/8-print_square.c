b617ae93b870e868e494a92f29975ad3fa2c6652
smythlamar <caltiesmith41@gmail.com>
2023-07-22 18:30:18 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - func print a square
+ * @size: size of the square
+ * Description: Can only use _putchar to print. Use '#' print square
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	y = 0;
+
+	if (size < 1)
+		_putchar('\n');
+
+	while (y < size)
+	{
+		x = 0;
+		while (x < size)
+		{
+			_putchar('#');
+			x++;
+		}
+		_putchar('\n');
+		y++
+	}
+}
