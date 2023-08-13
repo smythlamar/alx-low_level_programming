9529dd92e9fcb47ff3ae191cc7ced61918ba8dd2
smythlamar <caltiesmith41@gmail.com>
2023-08-13 12:40:14 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,7 +12,6 @@ int main(int argc, char *argv[])
 {
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[2]);
-	int result = num1 * num2;
 
 	if (argc != 3)
 	{
@@ -21,6 +20,8 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
+		int result = num1 * num2;
+
 		printf("%d\n", result);
 		return (0);
 	}
