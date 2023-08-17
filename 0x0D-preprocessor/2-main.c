ef2c2ba7720f106945787bdd210a548198f30fce
smythlamar <caltiesmith41@gmail.com>
2023-08-16 18:19:36 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#define FILE_NAME_H
+#include <stdio.h>
+
+/**
+ * main - provides name of file
+ *
+ * Return: Always 0(Success)
+ **/
+
+int main(void)
+{
+	printf("%s\n",__FILE__);
+	return (0);
+}
