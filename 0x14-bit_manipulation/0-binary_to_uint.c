c4b5dd4368e7fb719f5fe17cc0341ffc38d5f643
smythlamar <smythlamar@github.com>
2024-02-28 05:10:37 -0800

    cleared whitespaces

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
 #include "main.h"
+ /**
+  * main - print the binary representation of an number
+  *
+  * Return: Always (0)Success
+  **/
+int main()
+{
+	unsigned int binary_to_uint(const char *b);
+
+
+}
 
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,35 +1,34 @@
 #include "main.h"
 
 /**
- * _pow - func calculates (base ^ power)
- * @base: base of the exponent
- * @power: power of the exponent
- * 
- * Return: value of (base ^ power)
- * */
+* _pow - func calculates (base ^ power)
+* @base: base of the exponent
+* @power: power of the exponent
+* Return: value of (base ^ power)
+**/
 unsigned long int _pow(unsigned int base, unsigned int power)
 {
 	unsigned long int num;
 	unsigned int a;
-	
+
 	num = 1;
-	
+
 	for (a = 1; a <= power; a++)
 		num *= base;
 	return (num);
 }
 
 /**
- * print_binary - prints a number in binary notation
- * @n: number to print
- * 
- * Return: void
- * */
+* print_binary - prints a number in binary notation
+* @n: number to print
+*
+* Return: void
+**/
 void print_binary(unsigned long int n)
 {
 	unsigned long int divisor, check;
 	char flag;
-	
+
 	flag = 0;
 	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
 	while (divisor != 0)
