e79b6905a97a4c25ddf851c5266344ed2dd1fb26
smythlamar <smythlamar@github.com>
2024-02-29 04:57:29 -0800

    spaces removed

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,7 +6,7 @@
  * @n: number to check bits in
  * @index: index at which to check bit
  * Return: value of the bit, or -1 if there is an error
- **/i
+ **/
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int divisor, check;
